ba7ce614457ce2435b90dda2f1e5f311b332497f
mareboro <mareboro@o2.pl>
2026-05-03 19:29:17 +0100

    simple buttons in xaml

--- AspireFun/WpfApp/MainWindow.xaml.cs
+++ AspireFun/WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,10 +15,45 @@ namespace WpfApp;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
+    public int Counter { get; private set; } = 3;
+
+    public bool EnabledButtonTwo
+    {
+        get
+        {
+            if (Counter == 0) return true;
+            return Counter % 3 != 0;
+        }
+    }
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Remove_Two_OnClick(object sender, RoutedEventArgs e)
+    {
+        Add(-2);
+    }
+
+    private void Add_One_OnClick(object sender, RoutedEventArgs e)
+    {
+        Add(1);
+    }
+
+    private void Add(int number)
+    {
+        Counter += number;
+        OnPropertyChanged(nameof(Counter));
+        OnPropertyChanged(nameof(EnabledButtonTwo));
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
